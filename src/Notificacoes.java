cc1d19ffbe436197510745cdb59d7c664b7f9c20
Welton Oliveira <weltono143@gmail.com>
2025-06-23 00:20:28 -0300

    Correção na escrita de alguns dos códigos

--- src/Notificacoes.java
+++ src/Notificacoes.java
@@ -2,14 +2,17 @@ public class Notificacoes {
     public static void main (String[] args) {
 
         int notificacoes = 0;
-    for (int i = 1; i <= 5; i++) {
-        if (i % 2 == 0) {
-            notificacoes += 2;
-        } else {
-            notificacoes++;
+
+        for (int i = 1; i <= 5; i++) {
+            if (i % 2 == 0) {
+                notificacoes += 2;
+            } else {
+                notificacoes++;
+
+            }
+
         }
-        }
+        System.out.println(notificacoes);
+
     }
 }
-
-System.out.println(notificacoes);
